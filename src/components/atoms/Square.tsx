ee4974fb9bd55f2fcce78803063b6a461627d856
ikawheel <84578492+ikawheel@users.noreply.github.com>
2021-05-23 16:45:09 +0900

    マスをクリックするとXに変わるようにする

--- src/components/atoms/Square.tsx
+++ src/components/atoms/Square.tsx
@@ -1,5 +1,28 @@
-import { VFC } from "react";
+import { VFC, useState } from "react";
 
-const Square: VFC = () => <button className="square">{/* TODO */}a</button>;
+type dispCharType = {
+  num?: number;
+  str?: String;
+};
+const dispX: dispCharType = { str: "X" };
+
+const Square: VFC<{ val: number }> = ({ val }) => {
+  const dispNum: dispCharType = { num: val };
+  const [dispChar, setDispChar] = useState<dispCharType>(dispNum);
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        setDispChar(dispX);
+      }}
+    >
+      {
+        // null を確認すると意図通りに動かない。なにかしらライフサイクルの問題と思われる。
+        // dispChar.str ? dispChar.num : "";
+        dispChar.str ?? dispChar.num
+      }
+    </button>
+  );
+};
 
 export default Square;
--- src/components/molecules/Board.tsx
+++ src/components/molecules/Board.tsx
@@ -3,24 +3,25 @@ import Square from "../atoms/Square";
 
 const Game: VFC = () => {
   const status: String = "Next palayer: X";
+  // 縦横決め打ちなので、マス目作成をcontainerに作るとよりよいかも
   return (
     <>
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square val={1} />
+          <Square val={2} />
+          <Square val={3} />
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square val={4} />
+          <Square val={5} />
+          <Square val={6} />
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square val={7} />
+          <Square val={8} />
+          <Square val={9} />
         </div>
       </div>
     </>
