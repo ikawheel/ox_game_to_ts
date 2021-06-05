f856c32a6100c7c73cd4279c7eeefcc58524a768
ikawheel <84578492+ikawheel@users.noreply.github.com>
2021-06-05 17:01:35 +0900

    履歴ボタンを表示

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,20 +1,9 @@
 import React, { VFC } from "react";
 import Board from "./molecules/Board";
 
+// 整理した結果不要になってしまったGame・・・
 const Game: VFC = () => {
-  return (
-    <>
-      <div className="game">
-        <div className="gmae-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          {/* <div>{status}</div> */}
-          <div>{/* TODO */}</div>
-        </div>
-      </div>
-    </>
-  );
+  return <Board />;
 };
 
 export default Game;
--- src/components/molecules/Board.tsx
+++ src/components/molecules/Board.tsx
@@ -10,6 +10,14 @@ const Board: VFC = () => {
   const current = history[history.length - 1];
   const squares = current.squares.slice();
   const winner = calculateWinner(squares);
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button /*onClick={/*() => this.jumpTo(move)}*/>{desc}</button>
+      </li>
+    );
+  });
 
   let status;
   if (winner) {
@@ -21,22 +29,29 @@ const Board: VFC = () => {
   // 縦横決め打ちなので、マス目作成をcontainerに作るとよりよいかも
   return (
     <>
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          <Square val={0} setxIsNext={setxIsNext} />
-          <Square val={1} setxIsNext={setxIsNext} />
-          <Square val={2} setxIsNext={setxIsNext} />
+      <div className="game">
+        <div className="gmae-board">
+          <div className="board-row">
+            <Square val={0} setxIsNext={setxIsNext} />
+            <Square val={1} setxIsNext={setxIsNext} />
+            <Square val={2} setxIsNext={setxIsNext} />
+          </div>
+          <div className="board-row">
+            <Square val={3} setxIsNext={setxIsNext} />
+            <Square val={4} setxIsNext={setxIsNext} />
+            <Square val={5} setxIsNext={setxIsNext} />
+          </div>
+          <div className="board-row">
+            <Square val={6} setxIsNext={setxIsNext} />
+            <Square val={7} setxIsNext={setxIsNext} />
+            <Square val={8} setxIsNext={setxIsNext} />
+          </div>
         </div>
-        <div className="board-row">
-          <Square val={3} setxIsNext={setxIsNext} />
-          <Square val={4} setxIsNext={setxIsNext} />
-          <Square val={5} setxIsNext={setxIsNext} />
-        </div>
-        <div className="board-row">
-          <Square val={6} setxIsNext={setxIsNext} />
-          <Square val={7} setxIsNext={setxIsNext} />
-          <Square val={8} setxIsNext={setxIsNext} />
+        <div className="game-info">
+          <div>
+            <div className="status">{status}</div>
+            <ol>{moves}</ol>
+          </div>
         </div>
       </div>
     </>
